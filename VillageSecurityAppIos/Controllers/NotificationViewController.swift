93913714d241129c2834d2b5d299049d38fba257
Wachiraphong Prasoetwong <wachiraphong.pra@ascendcorp.com>
2018-02-14 17:40:56 +0700

    fix table height at 70

--- VillageSecurityAppIos/Controllers/NotificationViewController.swift
+++ VillageSecurityAppIos/Controllers/NotificationViewController.swift
@@ -16,18 +16,8 @@ class NotificationViewController: UIViewController, UITableViewDelegate, UITable
     let animals: [String] = ["Horse", "Cow", "Camel", "Sheep", "Goat"]
     
     // cell reuse id (cells that scroll out of view can be reused)
-    let cellReuseIdentifier = "cell"
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Register the table view cell class and its reuse id
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
-        
-        // (optional) include this line if you want to remove the extra empty cell divider lines
-        // self.tableView.tableFooterView = UIView()
-        
-        // This view controller itself will provide the delegate methods and row data for the table view.
         tableView.delegate = self
         tableView.dataSource = self
     }
@@ -36,18 +26,12 @@ class NotificationViewController: UIViewController, UITableViewDelegate, UITable
         super.didReceiveMemoryWarning()
     }
     
-    // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return self.animals.count
     }
     
-    // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        // create a new cell if needed or reuse an old one
-        let cell:NotificationTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! NotificationTableViewCell
-        
-        // set the text from the data model
+        let cell = self.tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier") as! NotificationTableViewCell
         cell.titleLabel?.text = self.animals[indexPath.row]
         
         return cell
@@ -56,6 +40,10 @@ class NotificationViewController: UIViewController, UITableViewDelegate, UITable
     // method to run when table view cell is tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         print("You tapped cell number \(indexPath.row).")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
     }
     
 }
