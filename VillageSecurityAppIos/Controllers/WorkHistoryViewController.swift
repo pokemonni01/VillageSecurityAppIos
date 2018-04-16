d0cf70d28736c7329bd899a4e3888f47d94f965f
Wachiraphong Prasoetwong <wachiraphong.pra@ascendcorp.com>
2018-04-16 12:50:10 +0700

    add request list village

--- VillageSecurityAppIos/Controllers/WorkHistoryViewController.swift
+++ VillageSecurityAppIos/Controllers/WorkHistoryViewController.swift
@@ -15,12 +15,25 @@ class WorkHistoryViewController: UIViewController {
     @IBOutlet weak var mTimePicker: UITextField!
     @IBOutlet weak var mDatePicker: UITextField!
     @IBOutlet weak var mBorderPicker: UITextField!
+    @IBOutlet weak var mVillagePicker: UITextField!
+    
+    @IBOutlet var rootView: UIView!
+    
+    private var mVillages: [Village]?
+    
+    private let mViewControllerUtils : ViewControllerUtils = ViewControllerUtils()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        mTimePicker.tintColor = .clear
-        mDatePicker.tintColor = .clear
-        mBorderPicker.tintColor = .clear
+//        mTimePicker.tintColor = .clear
+//        mDatePicker.tintColor = .clear
+//        mBorderPicker.tintColor = .clear
+        loadVillageList()
+    }
+    
+    func loadVillageList() {
+        mViewControllerUtils.showActivityIndicator(uiView: rootView)
+        ListVillageApi.requestListVillage(self)
     }
     
     override func didReceiveMemoryWarning() {
@@ -69,5 +82,39 @@ class WorkHistoryViewController: UIViewController {
             return
         }, cancel: { ActionMultipleStringCancelBlock in return }, origin: sender)
        
+    }
+    
+    
+    @IBAction func onVillagePickerClick(_ sender: Any) {
+        ActionSheetStringPicker.show(withTitle: "เลือกหมู่บ้าน", rows: mVillages?.map({ (village: Village) -> String in
+            village.name!
+        }), initialSelection: 0, doneBlock: {
+            picker, indexes, values in
+            print("values = \(String(describing: values))")
+            print("indexes = \(String(describing: indexes))")
+            print("picker = \(String(describing: picker))")
+            self.mVillagePicker.endEditing(true)
+            self.mVillagePicker.text = values as? String
+            return
+        }, cancel: { ActionMultipleStringCancelBlock in return }, origin: sender)
+    }
+}
+
+extension WorkHistoryViewController: ListVillageDelegate {
+    func onRequestListVillageSuccess(response: ListVillageResponse) {
+        mViewControllerUtils.hideActivityIndicator(uiView: rootView)
+        mVillages = response.village
+        mVillagePicker.text = mVillages?.first?.name
+        print(response)
+    }
+    
+    func onRequestListVillageFail(response: ListVillageResponse) {
+        mViewControllerUtils.hideActivityIndicator(uiView: rootView)
+        print(response)
+    }
+    
+    func onRequestListVillageError(title: String, message: String) {
+        mViewControllerUtils.hideActivityIndicator(uiView: rootView)
+        print(title+" "+message)
     }
 }
