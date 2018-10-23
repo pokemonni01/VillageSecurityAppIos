18cd4fe6b5bd71217ba2ce24cfc70a6b4e921937
Wachirapong Prasertwonr <oriounited@gmail.com>
2018-10-23 21:04:59 +0700

    finish draw ui

---
+++ VillageSecurityAppIos/Controllers/Report No Coupon/ReportNoCouponViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReportNoCouponViewController.swift
+//  Secure A
+//
+//  Created by Wachirapong on 23/10/2561 BE.
+//  Copyright © 2561 Wachirapong. All rights reserved.
+//
+
+import UIKit
+
+class ReportNoCouponViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
